f4dd8048cffae31675cca78fba7cb6fd3928224c
kennylam91 <phamvanlam1991@gmail.com>
2019-05-02 10:53:09 +0700

    first add

---
+++ puzzle_game.js
@@ -0,0 +1,49 @@
+let objImg1 = document.getElementById("img1");
+let objImg2 = document.getElementById("img2");
+let objImg3 = document.getElementById("img3");
+
+function imgswitch1() {
+
+    imgSwitch(objImg1);
+}
+
+function imgswitch2() {
+
+    imgSwitch(objImg2);
+}
+
+function imgswitch3() {
+
+    imgSwitch(objImg3);
+}
+
+function imgSwitch(objImg) {
+    let srcImg1 = "./1.png";
+    let srcImg2 = "./2.png";
+    let srcImg3 = "./3.png";
+
+    if (objImg.alt == "img1") {
+        objImg.src = srcImg2;
+        objImg.alt = "img2";
+    } else if (objImg.alt == "img2") {
+        objImg.src = srcImg3;
+        objImg.alt = "img3";
+    } else if (objImg.alt == "img3") {
+        objImg.src = srcImg1;
+        objImg.alt = "img1";
+    }
+    imgMatch();
+}
+
+function imgMatch() {
+    if ((objImg1.alt == objImg2.alt) && (objImg1.alt == objImg3.alt)) {
+        document.getElementById("div_to").style.border = "solid 5px blue";
+
+    } else {
+        document.getElementById("div_to").style.border = "none";
+    }
+}
+
+
+
+
